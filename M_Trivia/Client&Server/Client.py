cdcd5251ca1523c7b11ab365ae61821a1b2edc75
Cyber_User <yogevh16@gmail.com>
2025-04-03 18:41:57 +0300

    Client.py and Server.py Added

---
+++ M_Trivia/Client&Server/Client.py
@@ -0,0 +1,28 @@
+import socket
+
+PORT = 5554
+IP = "127.0.0.1"
+
+def create_client_socket(server_ip, port):
+    # Create a socket and connect to the server
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    return sock
+
+def communicate(client_socket):
+    # Send "Hello" and receive response
+    server_address = (IP, PORT)
+    client_socket.connect(server_address)
+
+
+def main():
+    #Client Build
+    client_socket = create_client_socket(IP, PORT)
+    communicate(client_socket)
+
+    for i in range(5):
+        client_socket.send(b"Hello")
+
+
+if __name__ == "__main__":
+    main()
+
