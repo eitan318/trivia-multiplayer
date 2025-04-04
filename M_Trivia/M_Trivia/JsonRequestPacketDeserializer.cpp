955cee39ade059d3de4d2e5fd4bddba68f4e9e8b
Cyber_User <yogevh16@gmail.com>
2025-04-04 19:45:25 +0300

    finished the jsonpacketDeserializer class

--- M_Trivia/M_Trivia/JsonRequestPacketDeserializer.cpp
+++ M_Trivia/M_Trivia/JsonRequestPacketDeserializer.cpp
@@ -3,13 +3,14 @@
 LoginRequest JsonRequestPacketDeserializer::deserializeLoginRequest(const vector<char> buffer)
 {
     int jsonLength = 0;
-    std::memcpy(&jsonLength, buffer.data() + 1, sizeof(int)); //get the size of the packet
+    std::memcpy(&jsonLength, buffer.data() + 1, sizeof(int)); //get the size of the packet into an int variable
 
-    std::string jsonmsg(buffer.begin() + 5, buffer.begin() + 5 + jsonLength);
+    std::string jsonmsg(buffer.begin() + 5, buffer.begin() + 5 + jsonLength); //get data as string
 
-    nlohmann::json j = nlohmann::json::parse(jsonmsg);
+    nlohmann::json j = nlohmann::json::parse(jsonmsg); //parse the data to json
 
     LoginRequest request;
+    //putting data in the login request variable
     request.username = j["username"];
     request.password = j["password"];
 
@@ -24,6 +25,7 @@ SignupRequest JsonRequestPacketDeserializer::deserializeSignUpRequest(const vect
     nlohmann::json j = nlohmann::json::parse(jsonmsg);
 
     SignupRequest request;
+    //putting data in the login request variable
     request.email = j["email"];
     request.username = j["username"];
     request.password = j["password"];
