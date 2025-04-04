519b745dc7b3008a4284722e5afad6e0016a4e28
Cyber_User <yogevh16@gmail.com>
2025-04-04 19:40:43 +0300

    finished implementing functions

--- M_Trivia/M_Trivia/JsonRequestPacketDeserializer.cpp
+++ M_Trivia/M_Trivia/JsonRequestPacketDeserializer.cpp
@@ -16,5 +16,17 @@ LoginRequest JsonRequestPacketDeserializer::deserializeLoginRequest(const vector
     return request;
 }
 SignupRequest JsonRequestPacketDeserializer::deserializeSignUpRequest(const vector<char> buffer) {
+    int jsonLength = 0;
+    std::memcpy(&jsonLength, buffer.data() + 1, sizeof(int)); //get the size of the packet
 
+    std::string jsonmsg(buffer.begin() + 5, buffer.begin() + 5 + jsonLength);
+
+    nlohmann::json j = nlohmann::json::parse(jsonmsg);
+
+    SignupRequest request;
+    request.email = j["email"];
+    request.username = j["username"];
+    request.password = j["password"];
+
+    return request;
 }
