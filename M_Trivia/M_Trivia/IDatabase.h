266bbd721177084c200931e3e9925f864d96ee12
Cyber_User <eitanforschool08@gmail.com>
2025-04-05 18:28:59 +0300

    Changed to const ref

--- M_Trivia/M_Trivia/IDatabase.h
+++ M_Trivia/M_Trivia/IDatabase.h
@@ -4,9 +4,9 @@ class IDatabase {
 public:
 	virtual bool open() = 0;
 	virtual bool close() = 0;
-	virtual int doesUserExist(std::string) = 0;
-	virtual int doesPasswordMatch(std::string, std::string) = 0;
-	virtual int addNewUser(std::string, std::string, std::string) = 0;
+	virtual int doesUserExist(const std::string&) = 0;
+	virtual int doesPasswordMatch(const std::string&, const std::string&) = 0;
+	virtual int addNewUser(const std::string&, const std::string&, const std::string&) = 0;
 	virtual bool createInitialDB() = 0;
 
 };
