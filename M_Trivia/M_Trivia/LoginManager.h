7d29e3a8c503a2d564ae8e41097fda17a9776088
Cyber_User <yogevh16@gmail.com>
2025-04-05 18:07:47 +0300

    Preped New classes

---
+++ M_Trivia/M_Trivia/LoginManager.h
@@ -0,0 +1,12 @@
+#pragma once
+//#include "IDataBase.h"
+#include "LoggedUser.h"
+#include <vector>
+
+class LoginManager {
+private:
+	//IDatabase m_database;
+	std::vector<LoggedUser> m_loggedUsers;
+public:
+
+};
---
+++ M_Trivia/M_Trivia/RequestHandlerFactory.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "LoginRequestHandler.h"
+
+class RequestHandlerFactory {
+private:
+	
+};
