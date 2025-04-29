67d070e4ac5196c1b1b6716a6b8369324cbad06d
Cyber_User <yogevh16@gmail.com>
2025-04-29 17:13:39 +0300

    Updated HighscoresPage UI and ViewModel

---
+++ ClientApp/ClientApp/ViewModels/HighScoresPageViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ClientApp.ViewModels
+{
+    class HighScoresPageViewModel : BaseViewModel
+    {
+        /// <summary>
+        /// The page that owns this ViewModel. Used for navigation purposes.
+        /// </summary>
+        private Page ownerPage;
+        private HighScoresPageViewModel(Page owner)
+        {
+            this.ownerPage = owner;
+        }
+        public static StatisticsPageViewModel Instance(Page owner)
+        {
+            return GetInstance(() => new HighScoresPageViewModel(owner));
+        }
+    }
+}
