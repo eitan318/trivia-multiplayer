9350a1b4dec855f4a594644b0f87faad641e85e1
Y H <yogevh16@gmail.com>
2025-05-31 19:50:36 +0300

    RoomMemberRequest handler

---
+++ M_Trivia/M_Trivia/RoomMemberRequestHandler.cpp
@@ -0,0 +1,65 @@
+#include "RoomMemberRequestHandler.hpp"
+#include "LeaveRoomResponse.hpp"
+#include "JsonResponsePacketSerializer.hpp"
+#include "RequestsCodes.hpp"
+#include "ServerErrorResponse.hpp"
+
+
+RoomMemberRequestHandler::RoomMemberRequestHandler(RequestHandlerFactory& handlerFactory,
+	LoggedUser loggedUser, Room* room) :
+	RoomRequestHandler(room,
+		loggedUser,
+		handlerFactory.getRoomManger(),
+		handlerFactory)
+{
+}
+
+RoomMemberRequestHandler::~RoomMemberRequestHandler() 
+{
+}
+
+bool RoomMemberRequestHandler::isRequestRelevant(const RequestInfo & requestInfo) const 
+{
+	switch (static_cast<RequestCodes>(requestInfo.code)) {
+	case RequestCodes::LeaveRoomRequest:
+	case RequestCodes::GetRoomStateRequest:
+		return true;
+	default:
+		return false;
+	}
+}
+
+RequestResult RoomMemberRequestHandler::handleRequest(const RequestInfo& requestInfo, SOCKET socket)
+{
+	try {
+		switch (static_cast<RequestCodes>(requestInfo.code)) {
+		case RequestCodes::LeaveRoomRequest:
+			return this->LeaveRoomRequest(requestInfo);
+		case RequestCodes::GetRoomStateRequest:
+			return this->getRoomState(requestInfo);
+		default:
+			ServerErrorResponse errorResponse("Invalid msg code.");
+			RequestResult requestResult(
+				JsonResponsePacketSerializer::serializeResponse(errorResponse),
+				nullptr);
+			return requestResult;
+		}
+	}
+	catch (const std::exception& e) {
+		ServerErrorResponse errResponse(e.what());
+		RequestResult res(
+			JsonResponsePacketSerializer::serializeResponse(errResponse), nullptr);
+		return res;
+	}
+}
+
+RequestResult RoomMemberRequestHandler::LeaveRoomRequest(RequestInfo requestInfo) 
+{
+	this->m_room->removeUser(this->m_user);
+	LeaveRoomResponse leaveRoomResponse(GENERAL_SUCCESS_RESPONSE_STATUS);
+	RequestResult result;
+	result.response = JsonResponsePacketSerializer::serializeResponse(leaveRoomResponse);
+	result.newHandler = this->m_requestHandlerFactory.createMenuRequestHandler(this->m_user);
+	return result;
+}
+
