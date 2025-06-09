e49cd1be44a214a089c1be26d6ea5660e7be631c
Y H <yogevh16@gmail.com>
2025-06-09 21:19:40 +0300

    finished requests in client

--- ClientApp/ClientApp/Models/Requests/SubmitAnswerRequest.cs
+++ ClientApp/ClientApp/Models/Requests/SubmitAnswerRequest.cs
@@ -3,6 +3,11 @@ namespace ClientApp.Models.Requests
 {
     class SubmitAnswerRequest : IRequest
     {
+        public uint correctAnswerId { get; set; }
+        public SubmitAnswerRequest(uint correctAnswerID)
+        {
+            this.correctAnswerId = correctAnswerID;
+        }
         public RequestsCodes GetCode() => RequestsCodes.SubmitAnswerRequest;
 
     }
