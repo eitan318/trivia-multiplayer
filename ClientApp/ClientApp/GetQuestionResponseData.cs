829d0c3e9ab0dbd7f4b8d9e1698ec8426ee0f527
Cyber_User <eitanforschool08@gmail.com>
2025-06-22 23:34:21 +0300

    Added missing files from last version

---
+++ ClientApp/ClientApp/GetQuestionResponseData.cs
@@ -0,0 +1,16 @@
+﻿using ClientApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientApp
+{
+    class GetQuestionResponseData
+    {
+        public uint QuestionNumber { get; set; }
+        public QuestionInfo Question {  get; set; }
+    }
+}
