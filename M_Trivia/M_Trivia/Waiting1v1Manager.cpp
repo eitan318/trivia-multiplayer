deafa22317f7df4f2ea4b981302c1323c42936c6
Cyber_User <eitanforschool08@gmail.com>
2025-06-22 16:38:52 +0300

    Created manager

---
+++ M_Trivia/M_Trivia/Waiting1v1Manager.cpp
@@ -0,0 +1,61 @@
+#include "Waiting1v1Manager.hpp"
+
+Waiting1v1Manager::Waiting1v1Manager()
+{
+}
+
+Waiting1v1Manager::~Waiting1v1Manager()
+{
+}
+
+
+Waiting1v1Manager& Waiting1v1Manager::getInstance()
+{
+    static Waiting1v1Manager instance;
+    return instance;
+}
+
+
+GeneralResponseErrors Waiting1v1Manager::joinWaitingList(const LoggedUser& loggedUser)
+{
+    GeneralResponseErrors errors;
+    std::lock_guard<std::mutex> lock(this->m_waitingListMutex);
+    this->m_waitingList.emplace(loggedUser, false);
+    return errors;
+}
+
+GeneralResponseErrors Waiting1v1Manager::leaveWaitingList(const LoggedUser& loggedUser)
+{
+    std::lock_guard<std::mutex> lock(this->m_waitingListMutex);
+
+    auto it = this->m_waitingList.find(loggedUser);
+    if (it != this->m_waitingList.end()) {
+        this->m_waitingList.erase(it);
+        return GeneralResponseErrors{}; // No error
+    }
+
+    return GeneralResponseErrors{ "User not found in waiting list" }; // Return error
+}
+
+std::pair<GeneralResponseErrors, bool> Waiting1v1Manager::didPlayerFoundMatch(const LoggedUser& loggedUser) const
+{
+    std::lock_guard<std::mutex> lock(this->m_waitingListMutex);
+
+    auto it = this->m_waitingList.find(loggedUser);
+    if (it != this->m_waitingList.end()) {
+        return { GeneralResponseErrors{}, it->second }; // No error, return match status
+    }
+
+    return { GeneralResponseErrors("User not found in waiting list"), false }; // Return error and default match status
+}
+
+
+
+//start game
+
+//GeneralResponseErrors errors;
+//std::shared_ptr<Game> game = createGame(this->defaultRoomPreview);
+//for (LoggedUser user : players) {
+//    game->join(user);
+//}
+//return errors;
