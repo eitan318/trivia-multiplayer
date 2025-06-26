e157511ce929ab4bd582e7ed9a9735de8ae63f6c
Cyber_User <eitanforschool08@gmail.com>
2025-06-26 12:02:09 +0300

    Organized the files in intuitive way

--- M_Trivia/M_Trivia/Response.hpp
+++ M_Trivia/M_Trivia/Response.hpp
@@ -1,44 +1,8 @@
 #pragma once
 #include "IResponseErrors.hpp"
 #include "json.hpp"
+#include "ResponseCodes.hpp"
 
-/**
- * @enum ResponseCodes
- * @brief Enumerates the response codes for different response types.
- */
-enum class ResponseCodes : unsigned char {
-    C_ErrorResponse = 0,
-    C_LoginResponse = 1,
-    C_GetRoomsResponse = 2,
-    C_GetPlayersInRoomResponse = 3,
-    C_GetHighScoreResponse = 4,
-    C_GetPersonalStatsResponse = 5,
-    C_SendPasswordResetCodeResponse = 6,
-    C_SignupResponse = 7,
-    C_ResetPasswordResponse = 8,
-    C_JoinRoomResponse = 9,
-    C_CreateRoomResponse = 10,
-    C_LogoutResponse = 11,
-    C_HighScoresRespones = 12,
-    C_VerifyPasswordResetCodeResponse = 13,
-    C_StartGameResponse = 14,
-    C_LeaveRoomResponse = 15,
-    C_GetRoomStatusResponse = 16,
-    C_CloseRoomResponse = 17,
-    C_GetQuestionResponse = 18,
-    C_LeaveGameResponse = 19,
-    C_GetGameResultsResponse = 20,
-    C_SubmitAnswerResponse = 21,
-    C_GetRoomStateResponse = 22,
-    C_GetGameStateResponse = 23,
-    C_Leave1v1WaitingListResponse = 24,
-    C_DidFound1v1MatchResponse = 25,
-    C_Join1v1WaitingListResponse = 26,
-    C_Get1v1GameSettingsResponse = 27,
-    C_AddQuestionResponse = 28,
-};
-
-#define GENERAL_SUCCESS_RESPONSE_STATUS 0
 
 template<ResponseCodes Code, typename ErrorType = IResponseErrors, typename DataType = std::monostate>
 class Response {
